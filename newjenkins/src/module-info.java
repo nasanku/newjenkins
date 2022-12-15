77909443002fe653f6d32bab1c75c0080138f401
nasanku <nasanku@INLEN8520043275.corp.capgemini.com>
2022-12-15 19:29:03 +0530

    commit here

---
+++ newjenkins/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nasanku
+ *
+ */
+module newjenkins {
+}
---
+++ newjenkins/src/newjenkinspac/Newjenkins.java
@@ -0,0 +1,10 @@
+package newjenkinspac;
+
+public class Newjenkins {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
